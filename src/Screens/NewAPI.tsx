63526007fe55dffcd508634cc378eee114ad19f7
Patel_ Gyanendra (gypatel@deloitte.com) <gypatel@your.hostname.com>
2022-05-16 19:31:34 +0530

    AddedSearchBar

--- src/Screens/NewAPI.tsx
+++ src/Screens/NewAPI.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Form, FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import NewsApiInterface from '../Interface/NewsApiInterface';
 
@@ -7,9 +7,9 @@ const NewAPI = () => {
 
     const [data, setData] = useState([])
     useEffect(() => {
-      getNews()
+        getNews()
     }, [data])
-    
+
 
     const getNews = () => {
         axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=7da6e2228bb743e4aea735b1ae40724d")
@@ -19,30 +19,35 @@ const NewAPI = () => {
             })
     }
 
-    const onClick = () => {
-
-    }
-
     return (
-        
+
         <div>
             <div className='container py-3'>
                 <h1 className='text-center'>News Feed</h1>
             </div>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            
             <div className="news-cards" >
                 {
-                    data.map((value : NewsApiInterface, index : number ) => {
+                    data.map((value: NewsApiInterface, index: number) => {
                         console.log(value);
                         return (
-                            
+
                             <Card style={{ width: '18rem', margin: "30px" }} key={index}  >
                                 {/* <Card.Img variant="top" src="{value.urlToImage}"/> */}
-                                <img src={value.urlToImage} alt="" className='card-img-top'/>
+                                <img src={value.urlToImage} alt="" className='card-img-top' />
                                 <Card.Body>
                                     <Card.Title>{value.title}</Card.Title>
                                     <Card.Text>
-                                               {value.content}
-                                
+                                        {value.content}
                                     </Card.Text>
                                     <Button variant="primary">Show Detail</Button>
                                 </Card.Body>
